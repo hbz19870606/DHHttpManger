6cd22fd751b0fe507595a993d47dbac5f38d453a
胡大海 <dahai.hu@fengjr.com>
2016-06-12 18:03:52 +0800

    删除无用文件

---
+++ DHHttpManger/DHHttpManger/DHHttpRequestManger.h
@@ -0,0 +1,13 @@
+//
+//  DHHttpRequestManger.h
+//  DHHttpManger
+//
+//  Created by 胡大海 on 16/6/12.
+//  Copyright © 2016年 胡大海. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DHHttpRequestManger : NSObject
+
+@end
--- DHHttpManger/DHHttpManger/DHHttpRequestManger.m
+++ DHHttpManger/DHHttpManger/DHHttpRequestManger.m
@@ -1,15 +1,15 @@
+
+
 //
-//  ViewController.h
+//  DHHttpRequestManger.m
 //  DHHttpManger
 //
 //  Created by 胡大海 on 16/6/12.
 //  Copyright © 2016年 胡大海. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "DHHttpRequestManger.h"
 
-@interface ViewController : UIViewController
-
+@implementation DHHttpRequestManger
 
 @end
-
