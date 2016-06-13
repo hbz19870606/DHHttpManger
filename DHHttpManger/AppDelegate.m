81054f498e8f03ef284de2cf7b9db2281c3e51d7
胡大海 <dahai.hu@fengjr.com>
2016-06-13 15:58:41 +0800

    test..

--- DHHttpManger/AppDelegate.m
+++ DHHttpManger/AppDelegate.m
@@ -16,7 +16,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+
+    
+    //Test
+    
+    
     return YES;
 }
 
